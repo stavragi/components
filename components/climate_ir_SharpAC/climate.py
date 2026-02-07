5535dc736b09d0311cc52b864b173952697fb471
stavragi <73437837+stavragi@users.noreply.github.com>
2026-02-07 14:06:42 +0200

    Update climate.py

--- components/climate_ir_SharpAC/climate.py
+++ components/climate_ir_SharpAC/climate.py
@@ -6,20 +6,16 @@ from esphome.const import CONF_MODEL
 
 AUTO_LOAD = ["climate_ir", "ir_remote_base"]
 
-panasonic_ns = cg.esphome_ns.namespace("panasonic")
-PanasonicClimate = panasonic_ns.class_("PanasonicClimate", climate_ir.ClimateIR)
+sharp_ns = cg.esphome_ns.namespace("sharp")
+SharpClimate = sharp_ns.class_("SharpClimate", climate_ir.ClimateIR)
 
-Model = panasonic_ns.enum("Model")
+Model = sharp_ns.enum("Model")
 MODELS = {
-    "LKE": Model.LKE,
-    "NKE": Model.NKE,
-    "DKE": Model.DKE,
-    "JKE": Model.JKE,
-    "CKP": Model.CKP,
-    "RKR": Model.RKR,
+    "A705": Model.A705,
+    "A903": Model.A903,
 }
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(PanasonicClimate).extend(
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(SharpClimate).extend(
     {
         cv.Required(CONF_MODEL): cv.enum(MODELS),
     }
@@ -30,4 +26,5 @@ async def to_code(config):
     ir_remote_base.load_ir_remote()
 
     var = await climate_ir.new_climate_ir(config)
+
     cg.add(var.set_model(config[CONF_MODEL]))
